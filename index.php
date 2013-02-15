2a4888ed33beebe9b8270cb062ade610050577f5
Jonic Linley <jonic@100yen.co.uk>
2013-02-15 21:04:49 +0000

    Added analytics and readied app for iOS home screen installation

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 	<head>
 		<meta charset="utf-8" />
 
-		<title>Pop - Prototype</title>
+		<title>Pop</title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
-		<link href="assets/styles/master.css?<?php echo time() ?>" rel="stylesheet" />
+		<link href="assets/images/icon.png" rel="apple-touch-icon" />
+		<link href="assets/styles/master.css" rel="stylesheet" />
 	</head>
 
 	<body>
@@ -17,10 +20,9 @@
 			<li class="tapY"></li>
 		</ul>
 
-		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-
 		<script src="assets/vendor/dat.gui.min.js"></script>
 		<script src="assets/vendor/ejohn.inheritance.js"></script>
+		<script src="assets/vendor/jquery-1.9.1.min.js"></script>
 		<script src="assets/vendor/requestAnimationFrame.js"></script>
 
 		<script src="assets/scripts/_utilities.js"></script>
@@ -34,5 +36,19 @@
 		<script src="assets/scripts/Screens.js"></script>
 
 		<script src="assets/scripts/_application.js"></script>
+
+		<script type="text/javascript">
+			var _gauges = _gauges || [];
+			(function() {
+				var t   = document.createElement('script');
+				t.type  = 'text/javascript';
+				t.async = true;
+				t.id    = 'gauges-tracker';
+				t.setAttribute('data-site-id', '511e9c71f5a1f51c4c000020');
+				t.src = '//secure.gaug.es/track.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(t, s);
+			})();
+		</script>
 	</body>
 </html>
