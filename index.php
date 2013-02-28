8d88d2f14205c81a3237ccd8233e2a2526536c12
Jonic Linley <jonic@100yen.co.uk>
2013-02-28 00:49:03 +0000

    Analytics are now only triggered if we're not in debug mode.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8" />
 
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="apple-mobile-web-app-title" content="Pop">
+		<meta name="apple-mobile-web-app-title" content="Pop" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
 		<meta name="viewport" content="user-scalable=no, initial-scale=1" />
@@ -29,18 +29,19 @@
 
 		<script src="assets/scripts/_application.js"></script>
 
-		<script type="text/javascript">
-			var _gauges = _gauges || [];
-			(function() {
-				var t   = document.createElement('script');
-				t.type  = 'text/javascript';
-				t.async = true;
-				t.id    = 'gauges-tracker';
-				t.setAttribute('data-site-id', '511e9c71f5a1f51c4c000020');
-				t.src = '//secure.gaug.es/track.js';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(t, s);
-			})();
+		<script>
+			if (!debug) {
+				var _gauges = _gauges || [];
+				(function() {
+					var t = document.createElement('script');
+					t.async = true;
+					t.id = 'gauges-tracker';
+					t.setAttribute('data-site-id', '511e9c71f5a1f51c4c000020');
+					t.src = '//secure.gaug.es/track.js';
+					var s = document.getElementsByTagName('script')[0];
+					s.parentNode.insertBefore(t, s);
+				}());
+			}
 		</script>
 	</body>
 </html>
