0ff8ae78ee9a9f4efe0c18ccf98781dbaeb1a3fa
Jonic Linley <jonic@100yen.co.uk>
2017-08-28 22:29:48 +0100

    Updated gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,104 +1,101 @@
+const autoprefixer = require('gulp-autoprefixer')
+const coffee = require('gulp-coffee')
+const concat = require('gulp-concat')
+const cssnano = require('gulp-cssnano')
+const gulp = require('gulp')
+const notify = require('gulp-notify')
+const sass = require('gulp-sass')
+const shell = require('gulp-shell')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const util = require('gulp-util')
 
-'use strict';
-
-var autoprefixer = require('gulp-autoprefixer');
-var gulp         = require('gulp');
-var coffee       = require('gulp-coffee');
-var concat       = require('gulp-concat');
-var notify       = require("gulp-notify");
-var util         = require('gulp-util');
-var rename       = require('gulp-rename');
-var sass         = require('gulp-sass');
-var shell        = require('gulp-shell');
-var sourcemaps   = require('gulp-sourcemaps');
-
-function handleError (error) {
-	error = error.toString();
-	util.log(error);
-	notify(error);
-	this.emit('end');
+const handleError = error => {
+  const formattedError = error.toString()
+  util.log(formattedError)
+  notify(formattedError)
+  this.emit('end')
 }
 
-var config = {
-	paths: {
-		scripts: {
-			files: {
-				any:       'assets/scripts/**/*.coffee',
-				utils:     'assets/scripts/utils.coffee',
-				helpers:   'assets/scripts/helpers/**/*.coffee',
-				classes:   'assets/scripts/classes/**/*.coffee',
-				scenes:    'assets/scripts/scenes/**/*.coffee',
-				entities:  'assets/scripts/entities/**/*.coffee',
-				bootstrap: 'assets/scripts/bootstrap.coffee'
-			},
-			dest: 'public/scripts'
-		},
-		styles: {
-			files: {
-				any:   'assets/styles/**/*.scss',
-				input: 'assets/styles/master.scss'
-			},
-			dest: 'public/styles'
-		}
-	},
-	targets: {
-		ios: 'Pop\ Rush/WebApp',
-	}
-};
+const config = {
+  paths: {
+    scripts: {
+      files: {
+        any: 'assets/scripts/**/*.coffee',
+        utils: 'assets/scripts/utils.coffee',
+        helpers: 'assets/scripts/helpers/**/*.coffee',
+        classes: 'assets/scripts/classes/**/*.coffee',
+        scenes: 'assets/scripts/scenes/**/*.coffee',
+        entities: 'assets/scripts/entities/**/*.coffee',
+        bootstrap: 'assets/scripts/bootstrap.coffee',
+      },
+      dest: 'public/scripts',
+    },
+    styles: {
+      files: {
+        any: 'assets/styles/**/*.scss',
+        input: 'assets/styles/master.scss',
+      },
+      dest: 'public/styles',
+    },
+  },
+}
 
-gulp.task('scripts', function () {
-	var files = config.paths.scripts.files;
+gulp.task('scripts', () => {
+  const files = config.paths.scripts.files
 
-	return gulp.src([
-			files.utils,
-			files.helpers,
-			files.classes,
-			files.scenes,
-			files.entities,
-			files.bootstrap
-		])
-		.on('error', handleError)
-		.pipe(sourcemaps.init())
-		.on('error', handleError)
-		.pipe(coffee({
-			bare: true
-		}))
-		.on('error', handleError)
-		.pipe(concat('application.js'))
-		.on('error', handleError)
-		.pipe(sourcemaps.write())
-		.on('error', handleError)
-		.pipe(gulp.dest(config.paths.scripts.dest))
-		.on('error', handleError);
-});
+  return gulp
+    .src([
+      files.utils,
+      files.helpers,
+      files.classes,
+      files.scenes,
+      files.entities,
+      files.bootstrap,
+    ])
+    .on('error', handleError)
+    .pipe(sourcemaps.init())
+    .on('error', handleError)
+    .pipe(coffee({ bare: true }))
+    .on('error', handleError)
+    .pipe(concat('application.js'))
+    .on('error', handleError)
+    .pipe(uglify())
+    .on('error', handleError)
+    .pipe(sourcemaps.write('.'))
+    .on('error', handleError)
+    .pipe(gulp.dest(config.paths.scripts.dest))
+    .on('error', handleError)
+})
 
-gulp.task('styles', function () {
-	var files = config.paths.styles.files;
+gulp.task('styles', () => {
+  const files = config.paths.styles.files
 
-	return gulp.src(files.input)
-		.pipe(sourcemaps.init())
-		.on('error', handleError)
-		.pipe(sass({
-			compressed: false
-		}))
-		.on('error', handleError)
-		.pipe(autoprefixer())
-		.on('error', handleError)
-		.pipe(sourcemaps.write())
-		.on('error', handleError)
-		.pipe(gulp.dest(config.paths.styles.dest))
-		.on('error', handleError);
-});
+  return gulp
+    .src(files.input)
+    .pipe(sourcemaps.init())
+    .on('error', handleError)
+    .pipe(
+      sass({
+        compressed: false,
+      }),
+    )
+    .on('error', handleError)
+    .pipe(autoprefixer())
+    .on('error', handleError)
+    .pipe(cssnano())
+    .on('error', handleError)
+    .pipe(sourcemaps.write('.'))
+    .on('error', handleError)
+    .pipe(gulp.dest(config.paths.styles.dest))
+    .on('error', handleError)
+})
 
-gulp.task('watch', function () {
-	gulp.watch(config.paths.scripts.files.any, ['scripts', 'deploy']);
-	gulp.watch(config.paths.styles.files.any,  ['styles',  'deploy']);
-});
+gulp.task('watch', () => {
+  gulp.watch(config.paths.scripts.files.any, ['scripts'])
+  gulp.watch(config.paths.styles.files.any, ['styles'])
+})
 
-gulp.task('deploy', shell.task('make deploy'));
+gulp.task('deploy', shell.task('make deploy'))
 
-gulp.task('default', [
-	'scripts',
-	'styles',
-	'watch'
-]);
+gulp.task('default', ['scripts', 'styles', 'watch'])
